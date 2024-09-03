f3d6e5c9875c0330caf2f521c1b1bd58e3904b0c
Kingsley Okpo <kngsley2018@gmail.com>
2024-09-03 11:11:41 +0100

    adds format method in filtered_logger.py

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+  #!/usr/bin/env python3
+"""this module handles the authentication of users"""
+
+from typing import  List, TypeVar
+from flask import  request
+
+class Auth:
+    """this class handles the authentication of users"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method that returns False - path and excluded_paths will
+         be used later, now, you don’t need to take care of them
+         """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """public method that returns None -
+        request will be the Flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method that returns None -
+        request will be the Flask request object
+        """
+        return request
