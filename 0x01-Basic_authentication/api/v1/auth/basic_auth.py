5a5211c18590ae5f1b9b2296244dd33b294f33c0
Kingsley Okpo <kngsley2018@gmail.com>
2024-09-04 01:59:19 +0100

    base64 auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 from typing import List, TypeVar
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ returns the decoded value of a
+        Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encode = base64.b64decode(base64_authorization_header)
+            return (encode.decode('utf-8'))
+        except base64.binascii.Error as e:
+            return None
