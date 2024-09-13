623c6d9b9f1abd96f2b130519ac7b8a8bce02829
Kingsley Okpo <kngsley2018@gmail.com>
2024-09-13 11:26:37 +0100

    returns the right status code

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -25,11 +25,9 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id == "me" and request.current_user is None:
-        abort(404)
-    if user_id == "me" and request.current_user is not None:
-        continue
     if user_id is None:
+        abort(404)
+    if user_id == "me" and request.current_user is None:
         abort(404)
     user = User.get(user_id)
     if user is None:
